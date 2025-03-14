ce323d9aaaa5d6cc82e6378a88b69b356134a648
SonOfUri <alotimothy01@gmail.com>
2025-03-14 13:45:47 +0100

    header icon size adjust

--- src/constants/config/headerNav.ts
+++ src/constants/config/headerNav.ts
@@ -1,8 +1,8 @@
 export const headerNav = [
     { name: "Dashboard", link: "/", icon: "/icons/profile.svg", width: "24" },
-    { name: "Markets", link: "/markets", icon: "/icons/markets.svg",  width: "27.93"  },
+    { name: "Markets", link: "/markets", icon: "/icons/markets.svg",  width: "24"  },
     // { name: "Rewards", link: "/rewards", icon: "/icons/rewards.svg",  width: "20.34" },
     // { name: "Leaderboard", link: "/leaderboard", icon: "/icons/leaderboard.svg",  width: "24"  },
     { name: "Withdraw", link: "/transact/withdraw", icon: "/icons/withdraw.svg",  width: "24"  },
-    { name: "Faucets", link: "https://faucets.vprotocol.xyz/", icon: "/icons/faucets.svg",  width: "27.93"  },
+    { name: "Faucets", link: "https://faucets.vprotocol.xyz/", icon: "/icons/faucets.svg",  width: "24"  },
   ];
