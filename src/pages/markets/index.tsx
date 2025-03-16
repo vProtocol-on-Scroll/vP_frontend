d9b11b10d597b9cac5645188ecf9eb1a993a3efc
Okste <okste1234@gmail.com>
2025-03-16 20:01:56 +0100

    FIX: apy at market place

--- src/pages/markets/index.tsx
+++ src/pages/markets/index.tsx
@@ -75,7 +75,7 @@ const Markets = () => {
 		.map((token) => ({
 			asset: token.name,
 			icon: token.icon,
-			supplyApy: `${(Number(token.supplyAPY) / 100000000).toFixed(2)}%`,
+			supplyApy: `${(Number(token.supplyAPY) / 10000).toFixed(2)}%`,
 			borrowApr: `${(Number(token.borrowAPR) / 10000).toFixed(2)}%`,
 			totalSupply: token.totalSupply.toString(),
 			totalSupplyUSD: `${token.supplyValueUSD.toFixed(2)}`,
